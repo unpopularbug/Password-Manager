01c3513b8f5434dce5aa2b0a1f4d957adb89e56d
Brian Mwangi <brianmwangikamau4@gmail.com>
2024-04-01 13:46:25 +0300

    Url route configuration

--- App/urls.py
+++ App/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewset, PasswordViewset
+from .views import UserViewset, LoginViewset, PasswordViewset
 
 router = DefaultRouter()
-router.register(r'accounts/user', UserViewset, basename='Create User')
-router.register(r'home', PasswordViewset, basename='Passwords')
+router.register(r'account/new-user', UserViewset, basename='Create User')
+# router.register(r'account/login', LoginViewset, basename='User Login')
+router.register(r'dashboard', PasswordViewset, basename='Dashboard')
+
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('account/login/', LoginViewset.as_view()),
 ]
