eb214139dbb07c65428241967ed40758d9794e62
Brian Mwangi <brianmwangikamau4@gmail.com>
2024-04-01 13:47:50 +0300

    Changed url routes

--- Manager/urls.py
+++ Manager/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('App.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/', include('App.urls')),
 ]
