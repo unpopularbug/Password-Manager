4417901bb94f4436d4dba54b4d24ee6d554368ed
Brian Mwangi <brianmwangikamau4@gmail.com>
2024-04-20 16:23:27 +0300

    Update: Changed the url configuration

--- App/urls.py
+++ App/urls.py
@@ -4,12 +4,11 @@ from rest_framework.routers import DefaultRouter
 from .views import UserViewset, LoginViewset, PasswordViewset
 
 router = DefaultRouter()
-router.register(r'account/new-user', UserViewset, basename='Create User')
-# router.register(r'account/login', LoginViewset, basename='User Login')
+router.register(r'accounts/signup', UserViewset, basename='Signup')
 router.register(r'dashboard', PasswordViewset, basename='Dashboard')
 
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('account/login/', LoginViewset.as_view()),
+    path('accounts/login/', LoginViewset.as_view(), name='Login'),
 ]
