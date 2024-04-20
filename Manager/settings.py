af7c77e4f6cbba15f2db30ae16001256afdfd760
Brian Mwangi <brianmwangikamau4@gmail.com>
2024-04-21 01:03:22 +0300

    Feature: Store static files with Whitenoise

--- Manager/settings.py
+++ Manager/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -103,8 +104,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_ROOT=os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 STATIC_URL = 'static/'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
