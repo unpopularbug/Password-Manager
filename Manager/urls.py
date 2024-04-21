20328fb4aabfefe2e34654e5a3496df77b36a2bd
Brian Mwangi <brianmwangikamau4@gmail.com>
2024-04-21 13:55:23 +0300

    Fix: Changed the urls config for additional security

--- Manager/urls.py
+++ Manager/urls.py
@@ -3,8 +3,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/', include('App.urls')),
+    path('api/v1/management/', admin.site.urls),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/', include('App.urls')),
 ]
