569385751ed94ef06e071af32655cf0c64ec1f3b
Brian Mwangi <brianmwangikamau4@gmail.com>
2024-04-21 13:05:42 +0300

    New: Configured urls for api user signup

--- App/urls.py
+++ App/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewset, LoginViewset, PasswordViewset
+from .views import UserViewset, LoginViewset, PasswordViewset, APIUserViewset
 
 router = DefaultRouter()
 router.register(r'accounts/signup', UserViewset, basename='Signup')
+router.register(r'accounts/api/signup', APIUserViewset, basename='API-user-signup')
 router.register(r'dashboard', PasswordViewset, basename='Dashboard')
 
 
