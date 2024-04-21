9ee47f3a8f14c25ea040381cd2121a729b12528b
Brian Mwangi <brianmwangikamau4@gmail.com>
2024-04-21 14:57:40 +0300

    Feature: Mapped a url to the logout function

--- App/urls.py
+++ App/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewset, LoginViewset, PasswordViewset, APIUserViewset
+from .views import UserViewset, LoginViewset, UserLogoutViewset, PasswordViewset, APIUserViewset
 
 router = DefaultRouter()
 router.register(r'accounts/signup', UserViewset, basename='Signup')
-router.register(r'accounts/api/signup', APIUserViewset, basename='API-user-signup')
+router.register(r'accounts/api-user/signup', APIUserViewset, basename='API-user-signup')
 router.register(r'dashboard', PasswordViewset, basename='Dashboard')
 
 
 urlpatterns = [
     path('', include(router.urls)),
     path('accounts/login/', LoginViewset.as_view(), name='Login'),
+    path('accounts/logout/', UserLogoutViewset.as_view(), name='Logout'),
 ]
