9552fb3ca5a859e7622b5712142d5db1ff02015a
Brian Mwangi <brianmwangikamau4@gmail.com>
2024-04-21 12:35:11 +0300

    Update: Registered models for API users and keys

--- App/admin.py
+++ App/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import CustomUser, Password
+from .models import CustomUser, Password, APIUser, APIKey
 
 admin.site.register(CustomUser)
 admin.site.register(Password)
+admin.site.register(APIUser)
+admin.site.register(APIKey)
