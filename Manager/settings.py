6f60a8df0bd09032adc198df9dce506ff1cdc402
Brian Mwangi <brianmwangikamau4@gmail.com>
2024-04-22 15:57:06 +0300

    New: Added a new installation for filtering

--- Manager/settings.py
+++ Manager/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'rest_framework_jwt',
     'rest_framework',
     'App',
