f65460df824f87f361935890d0330efa29a55deb
Brian Mwangi <brianmwangikamau4@gmail.com>
2024-04-26 23:40:30 +0300

    Update: Refactored and mapped new functionality to url routes

--- App/urls.py
+++ App/urls.py
@@ -4,13 +4,14 @@ from rest_framework.routers import DefaultRouter
 from .views import UserViewset, LoginViewset, LogoutViewset, PasswordViewset, APIUserViewset, PasswordResetView, PasswordConfirmView
 
 router = DefaultRouter()
-router.register(r'accounts/user', UserViewset, basename='custom user')
-router.register(r'accounts/api-user', APIUserViewset, basename='api user')
-router.register(r'dashboard/passwords', PasswordViewset, basename='dashboard')
+router.register(r'accounts/api-user', APIUserViewset, basename='register api user')
+router.register(r'dashboard/passwords', PasswordViewset, basename='passwords dashboard')
 
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('accounts/user/', UserViewset.as_view({'post': 'register'}), name='register user'),
+    path('accounts/user/<uuid:user_id>/confirm-code/', UserViewset.as_view({'post': 'confirm_code'}), name='confirm-code'),
     path('accounts/login/', LoginViewset.as_view(), name='login'),
     path('accounts/logout/', LogoutViewset.as_view(), name='logout'),
     path('accounts/reset-password/', PasswordResetView.as_view(), name='Reset password'),
