ad5da2dbccf4928c1ed798472255f270fbfc51c6
Brian Mwangi <brianmwangikamau4@gmail.com>
2024-04-26 19:34:56 +0300

    Update: Removed a static files configuration

--- Manager/settings.py
+++ Manager/settings.py
@@ -107,10 +107,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_ROOT = BASE_DIR / 'static'
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
