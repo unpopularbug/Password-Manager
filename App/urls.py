f70b09e348505f37098c5dcdcf09891714917144
Brian Mwangi <brianmwangikamau4@gmail.com>
2024-04-26 18:43:38 +0300

    Update: Mapped the password reset functions to url routes

--- App/urls.py
+++ App/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewset, LoginViewset, LogoutViewset, PasswordViewset, APIUserViewset
+from .views import UserViewset, LoginViewset, LogoutViewset, PasswordViewset, APIUserViewset, PasswordResetView, PasswordConfirmView
 
 router = DefaultRouter()
 router.register(r'accounts/user', UserViewset, basename='custom user')
@@ -13,4 +13,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('accounts/login/', LoginViewset.as_view(), name='login'),
     path('accounts/logout/', LogoutViewset.as_view(), name='logout'),
+    path('accounts/reset-password/', PasswordResetView.as_view(), name='Reset password'),
+    path('accounts/confirm-password-reset/', PasswordConfirmView.as_view(), name='Confirm password reset'),
 ]
