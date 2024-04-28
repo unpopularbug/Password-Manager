ab7e022f09dfaeefaa857d9b7d4d1c20590242a6
Brian Mwangi <brianmwangikamau4@gmail.com>
2024-04-28 22:31:21 +0300

    Returning objects' id on the admin panel

--- App/models.py
+++ App/models.py
@@ -174,3 +174,6 @@ class QuickTip(models.Model):
     id = models.AutoField(primary_key=True)
     created_at = models.DateTimeField(auto_now_add=True)
     text = models.CharField(max_length=200, null=True, blank=False)
+    
+    def __str__(self):
+        return f"Quick tip {self.id}"
